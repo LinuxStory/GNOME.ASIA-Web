3dc977e826f6aed566cfe0ba2355ca67e3a7528a
maxwell <liuxh@mikecrm.com>
2017-09-02 21:04:53 +0800

    first

--- web/application/controllers/Ticket.php
+++ web/application/controllers/Ticket.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Ticket extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -18,7 +18,7 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index()
+	public function loginByTicket()
 	{
 		$this->load->view('welcome_message');
 	}
